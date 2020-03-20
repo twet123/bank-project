48c01668b8bae28b669e9890da02089f10deeb88
Vlada <vladapopov02@gmail.com>
2020-03-20 19:37:56 +0100

    v0.0.1

---
+++ source/bank-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankproject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
