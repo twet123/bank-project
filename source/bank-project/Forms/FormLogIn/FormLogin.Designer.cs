c4192378254c24596f6df6a445bd89b58f9d1783
Vlada <vladapopov02@gmail.com>
2020-04-30 23:26:35 +0200

    v0.0.3

--- source/bank-project/Forms/FormLogIn/FormLogin.Designer.cs
+++ source/bank-project/Forms/FormLogIn/FormLogin.Designer.cs
@@ -7,9 +7,12 @@ namespace bankproject.Forms.FormLogIn
 {
     public partial class FormLogin : Form
     {
-        TextBox TbUsername;
-        PictureBox PbLogo;
-        PictureBox PbUsername;
+        TextBox TbUsername, TbPassword;
+        //PictureBox PbLogo;
+        PictureBox PbUsername, PbPassword;
+        Label LblHidden;
+        Panel Line1, Line2;
+        Button BtnLogin;
 
         public FormLogin()
         {
@@ -18,20 +21,136 @@ namespace bankproject.Forms.FormLogIn
 
         private void Render()
         {
+            //ctors
+            PbUsername = new PictureBox();
+            PbPassword = new PictureBox();
+            TbUsername = new TextBox();
+            TbPassword = new TextBox();
+            LblHidden = new Label();
+            Line1 = new Panel();
+            Line2 = new Panel();
+            BtnLogin = new Button();
+
             //FormLogin
-            this.Size = new Size(350, 400);
+            this.Size = new Size(372, 400);
             this.Font = new Font("Microsoft Sans Serif", 10);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.TopMost = true;
             this.WindowState = FormWindowState.Normal;
             this.FormBorderStyle = FormBorderStyle.None;
-            this.BackColor = Color.FromArgb(10, 41, 0);
-            this.Controls.Add(PbUsername);
+            //this.BackColor = Color.FromArgb(10, 41, 0);
+            this.BackColor = Color.White;
+            this.Load += Handle_Load;
 
             //PbUsername
-            PbUsername = new PictureBox();
-            PbUsername.ImageLocation = @"../../Assets/account_box-24px.svg";
-            PbUsername.Location = new Point(56, 94);
+            PbUsername.ImageLocation = @"../../Assets/account_box-24px.png";
+            PbUsername.Location = new Point(61, 94);
+            PbUsername.Size = new Size(24, 24);
+
+            //PbPassword
+            PbPassword.ImageLocation = @"../../Assets/vpn_key-24px.png";
+            PbPassword.Location = new Point(61, 150);
+            PbPassword.Size = new Size(24, 24);
+
+            //TbUsername
+            TbUsername.Location = new Point(100, 96);
+            TbUsername.BorderStyle = BorderStyle.None;
+            TbUsername.Size = new Size(200, 40);
+            TbUsername.Text = "Username";
+            TbUsername.ForeColor = Color.Silver;
+            TbUsername.Enter += TbUsername_Enter;
+            TbUsername.Leave += TbUsername_Leave;
+
+            //TbPassword
+            TbPassword.Location = new Point(100, 152);
+            TbPassword.BorderStyle = BorderStyle.None;
+            TbPassword.Size = new Size(200, 40);
+            TbPassword.Text = "Password";
+            TbPassword.ForeColor = Color.Silver;
+            TbPassword.Enter += TbPassword_Enter;
+            TbPassword.Leave += TbPassword_Leave;
+
+            //LblHidden
+            LblHidden.Location = new Point(-10, 0);
+            LblHidden.Size = new Size(0, 0);
+
+            //Line1
+            Line1.ForeColor = Color.Black;
+            Line1.BackColor = Color.Black;
+            Line1.Location = new Point(61, 124);
+            Line1.Size = new Size(250, 1);
+
+            //Line2
+            Line2.ForeColor = Color.Black;
+            Line2.BackColor = Color.Black;
+            Line2.Location = new Point(61, 180);
+            Line2.Size = new Size(250, 1);
+
+            //BtnLogin
+            BtnLogin.Location = new Point(61, 250);
+            BtnLogin.Size = new Size(250, 50);
+            BtnLogin.BackColor = Color.Black;
+            BtnLogin.ForeColor = Color.White;
+            BtnLogin.Text = "Log In";
+            BtnLogin.Font = new Font("Microsoft Sans Serif", 15);
+
+            //Adding controls
+            this.Controls.Add(PbUsername);
+            this.Controls.Add(PbPassword);
+            this.Controls.Add(TbUsername);
+            this.Controls.Add(TbPassword);
+            this.Controls.Add(LblHidden);
+            this.Controls.Add(Line1);
+            this.Controls.Add(Line2);
+            this.Controls.Add(BtnLogin);
+        }
+
+        void Handle_Load(object sender, EventArgs e)
+        {
+            LblHidden.Focus();
+        }
+
+        void TbUsername_Enter(object sender, EventArgs e)
+        {
+            if(TbUsername.Text == "Username")
+            {
+                TbUsername.Text = "";
+
+                TbUsername.ForeColor = Color.Black;
+            }
+        }
+
+        void TbUsername_Leave(object sender, EventArgs e)
+        {
+            if(TbUsername.Text == "")
+            {
+                TbUsername.Text = "Username";
+
+                TbUsername.ForeColor = Color.Silver;
+            }
+        }
+
+        void TbPassword_Enter(object sender, EventArgs e)
+        {
+            if(TbPassword.Text == "Password")
+            {
+                TbPassword.Text = "";
+
+                TbPassword.ForeColor = Color.Black;
+                TbPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        void TbPassword_Leave(object sender, EventArgs e)
+        {
+            if(TbPassword.Text == "")
+            {
+                TbPassword.Text = "Password";
+
+                TbPassword.ForeColor = Color.Silver;
+
+                TbPassword.UseSystemPasswordChar = false;
+            }
         }
 
     }
